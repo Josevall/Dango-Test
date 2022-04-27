c122074b8e7cf26135f05c4fdf70fa6c81b90808
Josevall <97254959+Josevall@users.noreply.github.com>
2022-04-27 05:17:47 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function ChangeColor(color) {
+    document.getElementById("change").innerHTML = "Added to cart!";
+}
+document.getElementById("change").onclick = function () {
+    ChangeColor();
+}
