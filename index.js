178375d990c06ae29b3c63d137d93f121ef6de14
Josevall <97254959+Josevall@users.noreply.github.com>
2022-04-28 12:40:40 -0400

    last one i think
    deeted cart icons

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 function changeText(cart) {
     cart.innerHTML = "Added!";
     cart.style.backgroundColor = "#8888";
-    var cartText = cart.previousSibling;
-    navigator.clipboard.writeText(cartText.textContent);
     setTimeout(function() {
       cart.innerHTML = "Add to cart";
+      cart.style.backgroundColor = "#acd9b2"
     }, 2000)
 }
 
+var btn = document.getElementById('btn');
+var title = document.getElementById('grid-container');
+var counter=0;
+
+function change(cart) {
+    if(counter==0) {
+        title.classList.add('change');
+        counter=1;
+    }
+    else {
+        title.classList.remove('change');
+        counter=0;
+    }
+}
+
+btn.addEventListener('click',change,true);
